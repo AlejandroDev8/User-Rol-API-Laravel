1917004d3d21c9c0bd43a3942239211a2912f8a1
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2025-04-29 01:11:19 -0600

    Refactor API routes to include user authentication and authorization endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,22 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/users', function () {
-    return "User List";
+Route::post('register', AuthController::class . '@register');
+Route::post('login', AuthController::class . '@login');
+
+Route::middleware(['auth:api'])->group(function () {
+    Route::get('user', AuthController::class . '@getProfile');
+
+    Route::middleware('role:admin|moderator')->group(function () {
+        Route::get('users', UserController::class . '@index');
+        Route::delete('users/{user}', UserController::class . '@destroy');
+    });
+
+    Route::middleware('role:admin|moderator|user')->group(function () {
+        Route::get('users/{user', UserController::class . '@show');
+        Route::put('users/{user}', UserController::class . '@update');
+    });
 });
